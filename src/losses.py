9e1e09e79ec9f40f6e9db800685980e7904e5b74
kkihara <kkihara1@gmail.com>
2017-05-04 22:47:13 -0700

    weighted logloss

--- src/losses.py
+++ src/losses.py
@@ -1,6 +1,17 @@
 from keras import backend as K
 
 
+def weighted_logloss(y_true, y_pred, zero_weight):
+    y_true_ = K.flatten(y_true)
+    y_pred_ = K.clip(K.flatten(y_pred), K.epsilon(), 1 - K.epsilon())
+
+    zero_mask = K.cast_to_floatx(K.equal(y_true_, 0) * zero_weight)
+    one_mask = K.cast_to_floatx(K.equal(y_true_, 1))
+    weights = zero_mask + one_mask
+
+    return -K.mean(y_true_ * K.log(y_pred_) + weights * (1 - y_true_) * K.log(1 - y_pred_))
+
+
 def jaccard(y_true, y_pred):
     y_true_ = K.flatten(y_true)
     y_pred_ = K.flatten(y_pred)
